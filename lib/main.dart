cb5c5097e80b543ce0d793888603e30ca721dc44
praveen <praveenpalleti70@gmail.com>
2024-06-17 22:11:15 +0530

    updated packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-import 'dart:developer';
+// import 'dart:developer';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_notification_channel/flutter_notification_channel.dart';
-import 'package:flutter_notification_channel/notification_importance.dart';
+// import 'package:flutter_notification_channel/flutter_notification_channel.dart';
+// import 'package:flutter_notification_channel/notification_importance.dart';
 
 import 'firebase_options.dart';
 import 'screens/splash_screen.dart';
@@ -51,11 +51,11 @@ class MyApp extends StatelessWidget {
 _initializeFirebase() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  var result = await FlutterNotificationChannel().registerNotificationChannel(
-      description: 'For Showing Message Notification',
-      id: 'chats',
-      importance: NotificationImportance.IMPORTANCE_HIGH,
-      name: 'Chats');
+  // var result = await FlutterNotificationChannel().registerNotificationChannel(
+  //     description: 'For Showing Message Notification',
+  //     id: 'chats',
+  //     importance: NotificationImportance.IMPORTANCE_HIGH,
+  //     name: 'Chats');
 
-  log('\nNotification Channel Result: $result');
+  // log('\nNotification Channel Result: $result');
 }
